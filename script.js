79504e213da6c3992ee4adb5bc76fee88eda80f4
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2025-04-22 01:55:21 +0600

    page2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var crsr =  document.querySelector("#cursor")
+var crsrb =  document.querySelector("#cursor-b")
+
+document.addEventListener("mousemove", function (dets){
+    crsr.style.left = dets.x+ "px"
+    crsr.style.top = dets.y+"px"
+    crsrb.style.left = dets.x - 150 + "px"
+    crsrb.style.top = dets.y - 150 + "px"
+})
+
+
+
+
+
+
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     height: "110px",
@@ -9,5 +25,17 @@ gsap.to("#nav", {
         end: "top -11%",
         scrub: 1,
 
+    }
+})
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2,
+      
     }
 })
