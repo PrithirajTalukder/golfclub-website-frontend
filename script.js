0f1c65d7d15df2de520e9be0d57208391f245092
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2025-04-24 02:54:36 +0600

    new file:   assets/Montserrat/Montserrat-Black.ttf new file:   assets/Montserrat/Montserrat-BlackItalic.ttf new file:   assets/Montserrat/Montserrat-Bold.ttf new file:   assets/Montserrat/Montserrat-BoldItalic.ttf new file:   assets/Montserrat/Montserrat-ExtraBold.ttf new file:   assets/Montserrat/Montserrat-ExtraBoldItalic.ttf new file:   assets/Montserrat/Montserrat-ExtraLight.ttf new file:   assets/Montserrat/Montserrat-ExtraLightItalic.ttf new file:   assets/Montserrat/Montserrat-Italic.ttf new file:   assets/Montserrat/Montserrat-Light.ttf new file:   assets/Montserrat/Montserrat-LightItalic.ttf new file:   assets/Montserrat/Montserrat-Medium.ttf new file:   assets/Montserrat/Montserrat-MediumItalic.ttf new file:   assets/Montserrat/Montserrat-Regular.ttf new file:   assets/Montserrat/Montserrat-SemiBold.ttf new file:   assets/Montserrat/Montserrat-SemiBoldItalic.ttf new file:   assets/Montserrat/Montserrat-Thin.ttf new file:   assets/Montserrat/Montserrat-ThinItalic.ttf new file:   assets/Montserrat/OFL.txt new file:   assets/card1.avif new file:   assets/card2.webp new file:   assets/card3.avif new file:   assets/favicon-32x32.png new file:   assets/page2-left.avif new file:   assets/page2-right.avif modified:   index.html modified:   script.js modified:   styles.css

--- script.js
+++ script.js
@@ -2,11 +2,34 @@ var crsr =  document.querySelector("#cursor")
 var crsrb =  document.querySelector("#cursor-b")
 
 document.addEventListener("mousemove", function (dets){
-    crsr.style.left = dets.x+ "px"
+    crsr.style.left = dets.x+30+ "px"
     crsr.style.top = dets.y+"px"
     crsrb.style.left = dets.x - 150 + "px"
     crsrb.style.top = dets.y - 150 + "px"
 })
+
+
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+
+            crsr.style.scale = 3
+            crsr.style.border = "1px solid #fff"
+            crsr.style.backgroundColor = "transparent"
+    }
+
+    )
+
+    elem.addEventListener("mouseleave", function(){
+
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #96c11E;"
+        crsr.style.backgroundColor = "#96c11E";
+})
+})
+
+
+
 
 
 
@@ -37,5 +60,62 @@ gsap.to("#main", {
         end: "top -70%",
         scrub: 2,
       
+    }
+})
+
+gsap.from( "#about-us img, #about-us-in",{
+    y:90,
+    opacity:0,
+    duration:1,
+    stagger: 0.1,
+    scrollTrigger:{
+        trigger: "#about-us",
+        scroller: "body",
+        start: "top 60%",
+        end:"top 55%",
+        scrub: 2
+
+    }
+
+} )
+
+
+gsap.from( "#cards-container",{
+    scale: 0.8,
+    opacity:0,
+    duration:1,
+    stagger: 0.1,
+    scrollTrigger:{
+        trigger: "#cards-container",
+        scroller: "body",
+        start: "top 70%",
+        end:"top 65%",
+        scrub: 1
+
+    }
+
+} )
+
+gsap.from("#col1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger: "#col1",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4
+    }
+})
+
+gsap.from("#col2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger: "#col1",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4
     }
 })
