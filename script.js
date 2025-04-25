5ff9845956773b67d1ae253c052cfa16db021dce
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2025-04-26 01:49:49 +0600

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var crsr =  document.querySelector("#cursor")
+//crsr.style.transition = "all ease 0.3s";
 var crsrb =  document.querySelector("#cursor-b")
+//crsrb.style.transition = "all ease 0.3s";
 
 document.addEventListener("mousemove", function (dets){
     crsr.style.left = dets.x+30+ "px"
@@ -9,24 +11,21 @@ document.addEventListener("mousemove", function (dets){
 })
 
 
-var h4all = document.querySelectorAll("#nav h4")
+var h4all = document.querySelectorAll("#nav h4, #footer h3, #footer #f1");
+
 h4all.forEach(function(elem){
     elem.addEventListener("mouseenter", function(){
-
-            crsr.style.scale = 3
-            crsr.style.border = "1px solid #fff"
-            crsr.style.backgroundColor = "transparent"
-    }
-
-    )
+        crsr.style.scale = 3;
+        crsr.style.border = "1px solid #fff";
+        crsr.style.backgroundColor = "transparent";
+    }); 
 
     elem.addEventListener("mouseleave", function(){
-
-        crsr.style.scale = 1
-        crsr.style.border = "0px solid #96c11E;"
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #96c11E"; 
         crsr.style.backgroundColor = "#96c11E";
-})
-})
+    }); 
+});
 
 
 
